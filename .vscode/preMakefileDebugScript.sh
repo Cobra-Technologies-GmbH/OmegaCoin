73a22adc455e2b62ac85ec937b94fede20213efb
Ing. Michael Goldfinger <mg@cobra.io>
2021-06-24 12:11:41 +0200

    New Settings

--- .vscode/preMakefileDebugScript.sh
+++ .vscode/preMakefileDebugScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clean -dxf
 ./autogen.sh
-./configure --with-incompatible-bdb --with-qrencode --with-zmq --with-gui --enable-lcov --enable-debug --enable-gprof
+./configure --with-incompatible-bdb --with-qrencode --with-zmq --with-gui --enable-lcov --enable-debug --enable-gprof --enable-extended-functional-tests --disable-hardening
 make clean
